8720b646a607ce8d7caf62a2cdf99bd3b9261764
Viktor Golovin <rinoally@gmail.com>
2023-07-07 22:16:59 +0300

    add commit

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "PUT"
+fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    body: JSON.stringify({
+        title: 'новая задача',
+        completed: false
+    }),
+    headers: {
+        'content-type': 'application/json'
+    }
 })
+    .then(Response => Response.json())
+    .then(json => console.log(json))
+
+
+//--------------------------
+
+const params = new URLSearchParams(location.search);
+
+const id = params.get('id');
+
+console.log(id);
+
+fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
     .then(Response => Response.json())
     .then(json => console.log(json))
